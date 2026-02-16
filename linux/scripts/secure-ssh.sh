11a4eac1ae9cd3759a092826a63c6092b1fab992
Jacob Greeley <jacob.greeley@mymail.champlain.edu>
2026-02-16 15:23:59 -0500

    Oops I dont know where this went

---
+++ linux/scripts/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author jacob-greeley
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+echo "ALL THE CODE GOES HERE"
