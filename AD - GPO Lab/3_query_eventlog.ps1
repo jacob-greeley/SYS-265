29b1466a155b76a5838488bd9861f0c5d5801f89
jacob-greeley <jacob.greeley@mymail.champlain.edu>
2026-03-02 11:32:46 -0500

    Update 3_query_eventlog.ps1

--- AD - GPO Lab/3_query_eventlog.ps1
+++ AD - GPO Lab/3_query_eventlog.ps1
@@ -2,8 +2,10 @@
 # Run on WKS01-jacob AFTER gpupdate /force and reboot
 # Deliverable 3: Queries System log for PuTTY GPO install confirmation
 
+
+Format-List
 Get-WinEvent -LogName System | Where-Object {
     $_.ProviderName -eq "Application Management Group Policy" -and
-    $_.Message -like "*PuTTY*Deploy SW*succeeded*"
+    $_.Message -like "*install*PuTTY*Deploy SW*succeeded*"
 } | Select-Object TimeCreated, Id, LevelDisplayName, Message |
 Format-List
